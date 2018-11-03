5a3adc01a9bcd118c280f43628086f0d18cef656
Ryan Scopio <ryanscopio@gmail.com>
2018-11-02 17:47:55 -0700

    Added Project

---
+++ Auto_2048/RyanIsCool/Program.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using System;
+
+namespace RyanIsCool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver("./");
+            driver.Navigate().GoToUrl("https://gabrielecirulli.github.io/2048/");
+
+            // 0: up, 1: right, 2: down, 3: left
+            Actions builder = new Actions(driver);
+
+            while (true)
+            {
+                //get board state
+                IWebElement tiles = driver.FindElement(By.ClassName("tile-container"));
+                 
+
+                //calculate move
+
+                //perform move
+                builder.SendKeys("s").Perform();
+                builder.SendKeys("d").Perform();
+
+                //check game lost
+
+            }
+        }
+    }
+}
